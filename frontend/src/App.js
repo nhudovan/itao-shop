2f243c7792ad7d409461c9bac55ffe6d255e7ff4
Du Dev <nhudo164@gmail.com>
2021-10-24 19:00:37 +0700

    header footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,25 @@
 
 
-import React from 'react'
+import React from 'react';
+import {Container} from 'react-bootstrap';
+import { Footer } from './component/Footer';
+import { Header } from './component/Header';
+
+
 
  const App=() =>{
   return (
     <div>
-      <h1>
-        Welcome to ITao Shop
-       </h1>
+      <Header/>
+      <main className="py-3">
+        <Container>
+            <h1>
+            Welcome to ITao Shop
+            </h1>
+        </Container>
+      
+       </main>
+       <Footer/>
     </div>
   )
 }
